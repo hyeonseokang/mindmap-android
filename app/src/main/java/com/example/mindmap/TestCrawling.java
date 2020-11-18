420bce36938cd548e2860e4852f0c89d9581f775
hyunseo <hansx2079@naver.com>
2020-11-18 14:30:30 +0900

    빽업

--- app/src/main/java/com/example/mindmap/TestCrawling.java
+++ app/src/main/java/com/example/mindmap/TestCrawling.java
@@ -1,11 +1,14 @@
 package com.example.mindmap;
 
+import android.util.Log;
+
 public class TestCrawling {
     public String testWord = "";
     public class WordThread extends CrawlingThread{
         public void CompleteCrawling(){
             // 반대말, 하위어, 참고 어휘, 비슷한말, 낮춤말, 상위어, 본말/준말, 높임말
             testWord = getSimilarWords().get("비슷한말").toString();
+            Log.d("비슷한말",testWord);
         }
     }
     public TestCrawling(){
